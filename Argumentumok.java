51d8a09008ed44187004127dca05a40c78b38ade
Hespi <sardi.andras@gmail.com>
2021-04-06 16:09:40 +0200

    Create Argumentumok.java

---
+++ Argumentumok.java
@@ -0,0 +1,10 @@
+public class Argumentumok {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < args.length; i+= 2) {
+            System.out.println(args[i]);
+        }
+  
+    }
+
+}
